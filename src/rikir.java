d63f5c2cd471c79c17e4f30c3353ae84c2217525
ESTUDIANTE <lopez.rikelmer@iemodeliaibague.edu.co>
2023-10-17 17:37:11 -0500

    Primera parte para crear arreglos en parqueadero

--- src/rikir.java
+++ src/rikir.java
@@ -4,12 +4,12 @@ public class rikir {
 
     public static void main(String[] args) {
 
-        String placa;
-        String nombre;
+        String placa ;
         double hora, resta, multi, horasalida;
         int tipo;
         int moto = 2;
         int carro = 1;
+        int nplacas;
 
 
         JOptionPane.showMessageDialog(null, "===========================================\n"+
@@ -18,20 +18,22 @@ public class rikir {
                 "Carro : 2000$ ---- Moto: 1000$ \n"+
                 "===========================================\n");
 
-        nombre = JOptionPane.showInputDialog("Digite su nombre");
-        placa = JOptionPane.showInputDialog("Digite su placa");
+        String [] ICarros = new String[10];
+        placa = (JOptionPane.showInputDialog("Digite su placa"));
+        for (int i=0;i<10;i++);
+        Double [] IhoraEntrada = new Double[10];
         hora = Double.parseDouble(JOptionPane.showInputDialog("Digite su hora de entrada"));
-
+        for (int i=0;i>10;i++);
         JOptionPane.showMessageDialog(null, "===========================================\n"+
                 "PARQUEADERO RikiRiki\n " +
                 "Valor hora o fraccion\n " +
                 "Carro: 2000 ---- Moto: 1000 \n" +
-                "El nombre del cliente es : " + nombre + "\n" +
                 "La placa es : " + placa + "\n" +
                 "Hora entrada : " + hora + "\n"+
                 "===========================================\n");
-
+        Double [] IhoraSalida = new Double[10];
         horasalida = Double.parseDouble(JOptionPane.showInputDialog("Digite la hora de la salida"));
+        for (int i=0;i>10;i++);
 
         resta = horasalida - hora;
 
@@ -57,7 +59,6 @@ public class rikir {
                          "PARQUEADERO RikiRiki \n " +
                          "Valor hora o fraccion \n " +
                          "Carro: 2000 ---- Moto: 1000 \n" +
-                         "El nombre del cliente es : " + nombre +"\n "+
                          "La placa es : " + placa + "\n" +
                          "Hora de entrada : " + hora + "\n" +
                          "Hora salida : " + horasalida + "\n" +
@@ -72,11 +73,14 @@ public class rikir {
                         "PARQUEADERO RikiRiki \n " +
                         "Valor hora o fraccion \n " +
                         "Carro: 2000 ---- Moto: 1000 \n" +
-                        "El nombre del cliente es : \n" + nombre +
-                        "La placa es : " + placa + "\n" +
-                        "Hora de entrada : " + hora + "\n" +
-                        "Hora salida : " + horasalida + "\n" +
-                        "Valor a pagar :" + multi + "\n" +
+                        "La placa es : " + placa + "\n")
+                        ;for (int i=0;i<10);
+
+                JOptionPane.showMessageDialog(null,"Hora de entrada: "+hora)
+                        for (int i=0;i>10);
+                JOptionPane.showMessageDialog(null,"Hora salida : " + horasalida + "\n");
+                         for (int i=0;i>10);
+                JOptionPane.showMessageDialog(null,"Valor a pagar :" + multi + "\n"+
                         "===========================================\n");
                         break;
             case 2:multi = Math.ceil(resta) * 1000;
@@ -84,7 +88,6 @@ public class rikir {
                         "PARQUEADERO RikiRiki \n " +
                         "Valor hora o fraccion \n " +
                         "Carro: 2000 ---- Moto: 1000 \n" +
-                        "El nombre del cliente es : " + nombre +"\n "+
                         "La placa es : " + placa + "\n" +
                         "Hora de entrada : " + hora + "\n" +
                         "Hora salida : " + horasalida + "\n" +
