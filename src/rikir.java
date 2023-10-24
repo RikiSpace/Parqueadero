cb92a266d6aa592fa3a3eeee3eadeafc2b78b5c0
ESTUDIANTE <lopez.rikelmer@iemodeliaibague.edu.co>
2023-10-24 17:25:55 -0500

    Adelanto de cilco for

--- src/rikir.java
+++ src/rikir.java
@@ -1,16 +1,14 @@
 import javax.swing.JOptionPane;
+import java.lang.reflect.Array;
 
 public class rikir {
 
     public static void main(String[] args) {
-
-        String placa ;
-        double hora, resta, multi, horasalida;
+        String placa;
+        double hora, resta , multi,horasalida, horaentrada;
+        String placa;
         int tipo;
-        int moto = 2;
-        int carro = 1;
-        int nplacas;
-
+        int carro=1,moto=2;
 
         JOptionPane.showMessageDialog(null, "===========================================\n"+
                 "Parqueadero RikiRiki \n" +
@@ -18,26 +16,7 @@ public class rikir {
                 "Carro : 2000$ ---- Moto: 1000$ \n"+
                 "===========================================\n");
 
-        String [] ICarros = new String[10];
-        placa = (JOptionPane.showInputDialog("Digite su placa"));
-        for (int i=0;i<10;i++);
-        Double [] IhoraEntrada = new Double[10];
-        hora = Double.parseDouble(JOptionPane.showInputDialog("Digite su hora de entrada"));
-        for (int i=0;i>10;i++);
-        JOptionPane.showMessageDialog(null, "===========================================\n"+
-                "PARQUEADERO RikiRiki\n " +
-                "Valor hora o fraccion\n " +
-                "Carro: 2000 ---- Moto: 1000 \n" +
-                "La placa es : " + placa + "\n" +
-                "Hora entrada : " + hora + "\n"+
-                "===========================================\n");
-        Double [] IhoraSalida = new Double[10];
-        horasalida = Double.parseDouble(JOptionPane.showInputDialog("Digite la hora de la salida"));
-        for (int i=0;i>10;i++);
-
-        resta = horasalida - hora;
-
-        tipo = Integer.parseInt(JOptionPane.showInputDialog("Digite 1 si es Carrro y 2 si es Moto"));
+        tipo = Integer.parseInt(JOptionPane.showInputDialog("Digite 1 para agregar Carrro, 2 para agregar Moto y 3 para salir"));
         /*if (tipo == carro) {
             multi = Math.ceil(resta) * 2000;
 
@@ -67,19 +46,41 @@ public class rikir {
     }*/
 
         switch (tipo) {
-            case 1:multi = Math.ceil(resta) * 2000;
+            case 1:
+                String [] IVehiculos = new String[10];
+                for (int i=0;i<10;i++);{
+                JOptionPane.showInputDialog("Digite su placa");
+            }
+            Array [] IhoraEntrada = new Double[10];
+            for (int i=0;i>10;i++){
+                JOptionPane.showInputDialog("Digite su hora de entrada");
+            }
+            Double [] IhoraSalida = new Double[10];
+            for (int i=0;i>10;i++){
+                JOptionPane.showInputDialog("Digite la hora de la salida");
+            }
+
 
                 JOptionPane.showMessageDialog(null, "===========================================\n"+
                         "PARQUEADERO RikiRiki \n " +
                         "Valor hora o fraccion \n " +
-                        "Carro: 2000 ---- Moto: 1000 \n" +
-                        "La placa es : " + placa + "\n")
-                        ;for (int i=0;i<10);
+                        "Carro: 2000 ---- Moto: 1000 \n ");
+                JOptionPane.showMessageDialog(null,"\n La placa es:" );
+            for (int i=0;i<10;i++);{
+                JOptionPane.showMessageDialog(null,(IhoraEntrada[1]);
+            }
 
-                JOptionPane.showMessageDialog(null,"Hora de entrada: "+hora)
-                        for (int i=0;i>10);
-                JOptionPane.showMessageDialog(null,"Hora salida : " + horasalida + "\n");
-                         for (int i=0;i>10);
+
+                JOptionPane.showMessageDialog(null, "Hora de entrada: " );
+            for (int i=0;i>10;i++);{
+                JOptionPane.showMessageDialog(null,(IhoraEntrada[i]));
+            }
+
+
+                JOptionPane.showMessageDialog(null, "Hora de salida: ");
+            for (int i=0;i>10;);{
+                JOptionPane.showMessageDialog(null,(IhoraSalida));
+            }
                 JOptionPane.showMessageDialog(null,"Valor a pagar :" + multi + "\n"+
                         "===========================================\n");
                         break;
@@ -88,6 +89,7 @@ public class rikir {
                         "PARQUEADERO RikiRiki \n " +
                         "Valor hora o fraccion \n " +
                         "Carro: 2000 ---- Moto: 1000 \n" +
+
                         "La placa es : " + placa + "\n" +
                         "Hora de entrada : " + hora + "\n" +
                         "Hora salida : " + horasalida + "\n" +
@@ -96,5 +98,6 @@ public class rikir {
                         break;
             default:JOptionPane.showMessageDialog(null,"La opcion de digito no esta en el rango");
         }
+    JOptionPane.showMessageDialog(null,"Gracias por utilizar nuestro parqueadero");
     }
 }
